06f65abbccbf41a94b9c9e090c31a6f92868ff94
Kaviprakash03 <110584501+Kaviprakash03@users.noreply.github.com>
2023-11-10 18:18:35 +0530

    generated the crud points for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_URL),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
