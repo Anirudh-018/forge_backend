bb806afff3f85b1ee75eeed71830560f63a60d47
Anirudh r k <menonani7@gmail.com>
2023-11-15 11:47:19 +0530

    changing routes

--- src/generate/generate.module.ts
+++ src/generate/generate.module.ts
@@ -3,10 +3,9 @@ import { MulterModule } from '@nestjs/platform-express';
 import { GenerateService } from './generate.service';
 import { GenerateController } from './generate.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { JwtService } from 'src/auth/jtw.service';
 @Module({
   controllers: [GenerateController],
-  providers: [GenerateService,JwtService],
+  providers: [GenerateService],
   imports: [
     MulterModule.register({
       dest: './uploads', // Set the destination folder for temporary storage
